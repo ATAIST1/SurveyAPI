eebd5b34c1281f1747c7f1ef918d5df2e412b33a
- <->
2024-02-08 11:37:31 +0600

    jhjhlih

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,10 @@
 package org.example;
 
+import org.example.entities.User;
+
 import java.sql.*;
+import java.util.ArrayList;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -8,15 +12,36 @@ public class Main {
         Connection con = null;
         ResultSet rs = null;
         Statement statement = null;
+        ArrayList<User> users = new ArrayList<User>();
         try {
+            Class.forName("org.postgresql.Driver");
             con = DriverManager.getConnection(conString, "postgres", "0000");
             statement= con.createStatement();
-            rs = statement.executeQuery("SELECT id,name,");
+            rs = statement.executeQuery("SELECT id,name,surname,username FROM users ORDER BY id");
 
+            while(rs.next()){
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String surname = rs.getString("surname");
+                String username = rs.getString(("username"));
+
+                User user = new User(id,name,surname,username);
+                users.add(user);
+
+            }
 
 
         }catch(SQLException e) {
-            System.out.println("cnnection server " + e.getMessage());
+            System.out.println("cnnection server: " + e.getMessage());
+        }catch(ClassNotFoundException e){
+            System.out.println("driver not found: " + e.getMessage());
         }
+
+        for(User user : users){
+            System.out.println(user);
+        }
+
+
+
     }
 }
