f4b45d8cf8a5ac0906d1f9d2415d995589ee5407
- <->
2024-02-08 10:42:52 +0600

    jhjh

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,12 +3,19 @@ package org.example;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 
 public class Main {
     public static void main(String[] args) {
         String conString = "jdbc:postgresql://localhost:5432/postgres";
+        Connection con = null;
+        ResultSet rs = null;
+        Statement statement = null;
         try {
-            Connection con = DriverManager.getConnection(conString, "postgres", "0000");
+            con = DriverManager.getConnection(conString, "postgres", "0000");
+            statement= con.createStatement();
         }catch(SQLException e) {
             System.out.println("cnnection server " + e.getMessage());
         }
