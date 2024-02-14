9616b012d09490f121f6b16ac72d59bb1b66a491
Ернар <230709@astanait.edu.kz>
2024-02-14 13:11:53 +0600

    participateSurvey method was slightly modified

--- src/main/java/org/example/Services/UserService.java
+++ src/main/java/org/example/Services/UserService.java
@@ -136,7 +136,14 @@ public class UserService {
                 System.out.println(survey);
             }
 
+            PreparedStatement preparedStatement = con.prepareStatement("INSERT INTO responses (response_id, survey_id, question_id, user_id, answer) VALUES (?, ?, ?, ?, ?)");
             System.out.println("Which survey you want to answer? (Enter survey_id) ");
+            int selectedSurveyId = scanner.nextInt();
+            for(Survey survey: surveysTable){
+                if(survey.getId() == selectedSurveyId) {
+
+                }
+            }
 
 
         }
